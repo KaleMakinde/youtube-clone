e8b03ced76927688e910b86af34497881204d6b1
KaleMakinde <makindeadeyemiadekale@gmail.com>
2023-08-25 15:34:12 +0100

    added components and sccs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react'
 
+import Header from './components/header/Header'
+import Sidebar from './components/sidebar/Sidebar'
+import { Container } from 'react-bootstrap'
+import HomeScreen from './components/screens/homeScreen/HomeScreen'
+
 const App = () => {
   return (
-    <div>
-      Not Youtube
+    <>
+    <Header/>
+    <div className="app_container border border-info">
+         <Sidebar/>
+
+         <Container fluid className='app__main border border-warning'>
+            <HomeScreen/>
+
+         </Container>
     </div>
+    </>
   )
 }
 
