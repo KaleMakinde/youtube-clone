a8989e78efce5e620c72a7876f13ba4e47293759
KaleMakinde <makindeadeyemiadekale@gmail.com>
2023-08-30 21:54:49 +0100

    added styles to categories bar and video component

--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -3,8 +3,11 @@ import "./_video.scss";
 
 const Video = () => {
   return (
-    <div>
-      Video
+    <div className="video">
+      <div className="video__top"></div>
+      <div className="video__title"></div>
+      <div className="video__details"></div>
+      <div className="video__channnel"></div>
     </div>
   )
 }
