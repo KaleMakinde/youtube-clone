e0cd307a6bdb7da96cfb74d2cd025a5a07d8f7bc
KaleMakinde <makindeadeyemiadekale@gmail.com>
2023-09-09 23:13:22 +0100

    added routes app

--- src/App.js
+++ src/App.js
@@ -1,38 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Header from './components/header/Header';
+import Sidebar from './components/sidebar/Sidebar';
+import { Container } from 'react-bootstrap';
+import HomeScreen from './components/screens/homeScreen/HomeScreen';
+import LoginScreen from './components/screens/homeScreen/loginScreen/loginScreen';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './_app.scss';
 
-import Header from './components/header/Header'
-import Sidebar from './components/sidebar/Sidebar'
-import { Container } from 'react-bootstrap'
-import HomeScreen from './components/screens/homeScreen/HomeScreen'
-
-import "./_app.scss"
-import LoginScreen from './components/screens/homeScreen/loginScreen/loginScreen'
-
-const App = () => {
-
-
-    const [sidebar, togglesidebar] = useState(false);
-
-
-    const handleToggleSidebar = () => togglesidebar(value=>!value)
-
+const Layout = ({ children }) => {
+  const [sidebar, toggleSidebar] = useState(false);
+  const handleToggleSidebar = () => toggleSidebar(value => !value);
 
   return (
-    // <>
-    // <Header handleToggleSidebar={handleToggleSidebar}/>
-    // <div className="app__container border border-info">
-    //      <Sidebar 
-    //      sidebar={sidebar}
-    //      handleToggleSidebar={handleToggleSidebar}
-    //      />
+    <>
+      <Header handleToggleSidebar={handleToggleSidebar} />
+      <div className="app__container border border-info">
+        <Sidebar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar} />
+        <Container fluid className="app__main border border-warning">
+          {children}
+        </Container>
+      </div>
+    </>
+  );
+};
 
-    //      <Container fluid className='app__main border border-warning'>
-    //         <HomeScreen/>
-    //      </Container>
-    // </div>
-    // </>
-    <LoginScreen/>
-  )
-}
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomeScreen />} /> {/* Use index property here */}
+        </Route>
 
-export default App
+        <Route path="/auth" element={<LoginScreen />} />
+
+        <Route path="/search" element={<Layout />}>
+          <Route index element={<h1>Search Results</h1>} /> {/* Use index property here */}
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
